5bb0f283c0dbb49950f5828214818114d5fcf0f0
victor-kiptoo-001 <victor.kiptoo1@student.moringaschool.com>
2024-02-21 07:46:16 +0300

    Updated functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { UserProvider } from './components/UserContext';
+
+import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import MyEditor from "./components/MyEditor";
+import SingleBlog from "./components/SingleBlog";
+import './App.css'
+import BlogPage from "./components/BlogPage";
+import UserProfile from "./components/UserProfile";
+import AboutUser from "./components/AboutUser";
+import ReviewSection from "./components/ReviewSection";
+import AdminDashboard from "./components/AdminDashboard";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (   
+    <UserProvider>
+      <div className='max-w-[1440px] mx-auto'>
+        <Header />
+        
+
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/sign_up" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/write" element={<MyEditor />} />
+            <Route path="/blogs" element={<BlogPage />} />
+            <Route path="/profile" element={<UserProfile />} />
+            <Route path="/admin-dashboard" element={<AdminDashboard/>} />
+            <Route path="/about" element={<AboutUser />} />
+
+            <Route path="/comments" element={<ReviewSection />} />
+            <Route path="/edit-content/:content_id" component={<MyEditor/>} />
+
+
+            <Route path="/content/:id" element={<SingleBlog/>} />
+            {/* <Route path="/content/:id" render={(props) => <SingleBlog {...props} />} /> */}
+
+
+            
+          </Routes>
+        </main>
+        
+        {/* <SingleBlog/> */}
+        <Footer />
+      </div>
+    </UserProvider>
   );
 }
 
